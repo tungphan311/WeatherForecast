a5d1ba111e174c9149e891696bc21346f12c5ad6
Phan Thanh Tung <a1midas@outlook.com>
2019-04-12 12:20:39 +0700

    redesign today fragment

--- app/src/main/java/com/example/weatherforecast/TodayFragment.java
+++ app/src/main/java/com/example/weatherforecast/TodayFragment.java
@@ -28,7 +28,8 @@ import java.util.Date;
  * A simple {@link Fragment} subclass.
  */
 public class TodayFragment extends Fragment {
-    TextView txtName, txtCurrentDate, txtTemp, txtHumidity, txtPressure, txtWind;
+    TextView txtName, txtCurrentDate, txtTemp, txtHumidity, txtPressure, txtWind, txtVisibility;
+    TextView txtSunset, txtSunrise, txtDescription;
     ImageView imgWeatherIcon;
 
     public TodayFragment() {
@@ -55,6 +56,10 @@ public class TodayFragment extends Fragment {
         txtHumidity = view.findViewById(R.id.hum);
         txtPressure = view.findViewById(R.id.pressure);
         txtWind = view.findViewById(R.id.wind);
+        txtVisibility = view.findViewById(R.id.visibility);
+        txtSunset = view.findViewById(R.id.sunset);
+        txtSunrise = view.findViewById(R.id.sunrise);
+        txtDescription = view.findViewById(R.id.description);
     }
 
     public void GetCurrentWeather(String data) {
@@ -87,17 +92,47 @@ public class TodayFragment extends Fragment {
 
                             JSONObject jsonObjectMain = jsonObject.getJSONObject("main");
                             String temp = jsonObjectMain.getString("temp");
+                            String maxTemp = jsonObjectMain.getString("temp_max");
+                            String minTemp = jsonObjectMain.getString("temp_min");
                             String humidity = jsonObjectMain.getString("humidity");
                             String pressure = jsonObjectMain.getString("pressure");
                             Double dou = Double.valueOf(temp);
                             String Temp = String.valueOf(dou.intValue());
+                            Double max = Double.valueOf(maxTemp);
+                            String MaxTemp = String.valueOf(max.intValue());
+                            Double min = Double.valueOf(minTemp);
+                            String MinTemp = String.valueOf(min.intValue());
                             txtTemp.setText(Temp);
                             txtHumidity.setText(humidity + "%");
-                            txtPressure.setText(pressure);
+                            txtPressure.setText(pressure + " hPa");
 
                             JSONObject jsonObjectWind = jsonObject.getJSONObject("wind");
                             String speed = jsonObjectWind.getString("speed");
                             txtWind.setText(speed + "m/s");
+
+                            String visibility = jsonObject.getString("visibility");
+                            int visi = Integer.valueOf(visibility);
+                            float tamnhin = visi / 1000;
+                            String Visiblity = String.valueOf(tamnhin);
+                            txtVisibility.setText(Visiblity + " km");
+
+                            JSONObject jsonObjectSys = jsonObject.getJSONObject("sys");
+                            String sunrise = jsonObjectSys.getString("sunrise");
+                            long rise = Long.valueOf(sunrise);
+                            Date dRise = new Date(rise*1000L);
+                            SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+                            String Sunrise = format.format(dRise);
+                            txtSunrise.setText(Sunrise);
+
+                            String sunset = jsonObjectSys.getString("sunset");
+                            long set = Long.valueOf(sunset);
+                            Date dSet = new Date(set*1000L);
+                            SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
+                            String SunSet = dateFormat.format(dSet);
+                            txtSunset.setText(SunSet);
+
+                            txtDescription.setText("Nhiệt độ cao nhất sẽ là: " + MaxTemp + (char)0x00B0 + "C. Nhiệt độ thấp nhất sẽ là: "
+                                                    + MinTemp + (char)0x00B0 + "C.");
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
